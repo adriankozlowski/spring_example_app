fa614158d887c2d64ae3c6d18659cb47f6c741b9
Adrian Kozłowski <adrian.kozlowski@outlook.com>
2018-03-25 14:12:50 +0200

    index

--- src/main/java/pl/sda/store/store/controllers/ItemController.java
+++ src/main/java/pl/sda/store/store/controllers/ItemController.java
@@ -1,14 +1,20 @@
 package pl.sda.store.store.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import pl.sda.store.store.model.Item;
+import pl.sda.store.store.services.ItemService;
 
 @Controller
 public class ItemController {
+
+
+    @Autowired
+    private ItemService itemService;
 
     @GetMapping("/additem")
     public String addItemForm(ModelMap map){
@@ -18,7 +24,7 @@ public class ItemController {
 
     @PostMapping("/additem")
     public String addItem(@ModelAttribute Item item) {
-
-        return "index";
+        itemService.addItem(item);
+        return "redirect:/";
     }
 }
