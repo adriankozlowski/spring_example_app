5ded7f8446c4dabe770218bc0702b0a0bd70ec4b
Adrian Kozłowski <adrian.kozlowski@outlook.com>
2018-03-25 12:27:25 +0200

    repozytorium item

--- src/main/java/pl/sda/store/store/repositories/ItemRepository.java
+++ src/main/java/pl/sda/store/store/repositories/ItemRepository.java
@@ -1,7 +1,36 @@
 package pl.sda.store.store.repositories;
 
 import org.springframework.stereotype.Repository;
+import pl.sda.store.store.model.Item;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class ItemRepository {
+    private final List<Item> db = new ArrayList<Item>();
+
+    /**
+     * Metoda dodaje dane do naszej bazy danych
+     * @param item
+     */
+    public void add(Item item ){
+        db.add(item);
+    }
+
+    /**
+     * Metoda usuwa dane z naszej bazy dancyh
+     * @param item
+     */
+    public void remove(Item item){
+        db.remove(item);
+    }
+
+    /**
+     * metoda zwraca wszystkie elementy w naszej bazie
+     * @return
+     */
+    public List<Item> getList(){
+        return db;
+    }
 }
